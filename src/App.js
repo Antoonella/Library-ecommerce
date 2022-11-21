d585781f169a66d5b4436e53bca05b84a9538556
antonella <antonella.makahe@gmail.com>
2022-11-21 16:37:00 +1100

    adding Book Router with BookInfo page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from './data'
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
     <div className="App">
      <Nav />
      <Route path="/" exact component={Home} />
-     <Route path="/books" render={() => <Books books={books} />} />
+     <Route path="/books" exact render={() => <Books books={books} />} />
+     <Route path="/books/:id" render={() => <BookInfo books={books} />} />
      <Footer />
     </div>
     </Router>
   );
 }
 export default App;
+ 
